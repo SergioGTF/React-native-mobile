b7eeab97c170d4d71df385626dbe199f7372ce60
SergioGTF <sergio23300033@aluno.cesupa.br>
2024-08-21 22:19:51 -0300

    React Native

--- meu-projeto/App.js
+++ meu-projeto/App.js
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Tittle from './src/components/Title';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
+      <Tittle/>
     </View>
   );
 }
